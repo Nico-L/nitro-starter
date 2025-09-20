748a8e142b733a5ec86e7b0dc2dd344ab6523f24
Nico-L <luchier@gmail.com>
2025-09-20 22:47:45 +0200

    test baserow env

--- server/api/index.ts
+++ server/api/index.ts
@@ -1,12 +1,12 @@
 import { Repo } from '~/types'
 
 export default defineEventHandler(async (event) => {
-  const data = await $fetch<Repo[]>(process.env.BASEROW_URL, {
+  /*const data = await $fetch<Repo[]>(process.env.BASEROW_URL, {
     method: 'GET',
     headers: {
       'Authorization': 'Token ' + process.env.BASEROW_DEVOIRS
     }
-  })
+  }) */
 
-  return data
+  return process.env.BASEROW_URL
 })
