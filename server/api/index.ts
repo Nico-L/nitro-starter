7faef480eb26452d66fc31fa7ec688f08713e6de
Nico-L <luchier@gmail.com>
2025-09-20 22:49:41 +0200

    test baserow url correcte

--- server/api/index.ts
+++ server/api/index.ts
@@ -1,12 +1,12 @@
 import { Repo } from '~/types'
 
 export default defineEventHandler(async (event) => {
-  /*const data = await $fetch<Repo[]>(process.env.BASEROW_URL, {
+  const data = await $fetch<Repo[]>(process.env.BASEROW_URL+'669051/?user_field_names=true', {
     method: 'GET',
     headers: {
       'Authorization': 'Token ' + process.env.BASEROW_DEVOIRS
     }
-  }) */
+  })
 
-  return process.env.BASEROW_URL
+  return data
 })
