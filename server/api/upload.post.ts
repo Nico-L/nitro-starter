d1ff3eaa0b5571d9336896a6f8edc86d968e2eaf
Nico-L <luchier@gmail.com>
2025-09-21 23:31:23 +0200

    uplozad

--- server/api/upload.post.ts
+++ server/api/upload.post.ts
@@ -3,8 +3,8 @@ import { Repo } from '~/types'
 export default defineEventHandler(async (event) => {
   const body = await readBody(event)
     var dataFile = new FormData()
-    dataFile.append('file', body.file)
-    console.log('datafile', dataFile)
+    dataFile.append('file', body)
+    console.log('datafile', body)
   const data = await $fetch<Repo[]>('https://api.baserow.io/api/user-files/upload-file/', {
     method: 'POST',
     headers: {
