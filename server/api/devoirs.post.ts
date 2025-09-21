140ea698e8b6585f95945cef80fc2e93b2d06286
Nico-L <luchier@gmail.com>
2025-09-21 17:01:46 +0200

    debug post

--- server/api/devoirs.post.ts
+++ server/api/devoirs.post.ts
@@ -1,13 +1,13 @@
 import { Repo } from '~/types'
 
 export default defineEventHandler(async (event) => {
-    const query = getQuery(event);
+  const body = await readBody(event)
   const data = await $fetch<Repo[]>(process.env.BASEROW_URL+'669051/?user_field_names=true', {
     method: 'POST',
     headers: {
       'Authorization': 'Token ' + process.env.BASEROW_DEVOIRS
     },
-    body: query
+    body: body
   })
 
   return query
