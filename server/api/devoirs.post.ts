fd2785b50d8cbd07b677d71d43d8a6e9d2b1d609
Nico-L <luchier@gmail.com>
2025-09-21 16:51:34 +0200

    debut post devoirs 3

--- server/api/devoirs.post.ts
+++ server/api/devoirs.post.ts
@@ -3,10 +3,11 @@ import { Repo } from '~/types'
 export default defineEventHandler(async (event) => {
     const query = getQuery(event);
   const data = await $fetch<Repo[]>(process.env.BASEROW_URL+'669051/?user_field_names=true', {
-    method: 'GET',
+    method: 'POST',
     headers: {
       'Authorization': 'Token ' + process.env.BASEROW_DEVOIRS
-    }
+    },
+    body: query
   })
 
   return query
