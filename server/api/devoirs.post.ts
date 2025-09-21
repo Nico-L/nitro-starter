7b77c71963393019537904aa6e471c8fc2b24107
Nico-L <luchier@gmail.com>
2025-09-21 16:46:56 +0200

    debut event

--- server/api/devoirs.post.ts
+++ server/api/devoirs.post.ts
@@ -1,6 +1,7 @@
 import { Repo } from '~/types'
 
 export default defineEventHandler(async (event) => {
+    const query = getQuery(event);
   const data = await $fetch<Repo[]>(process.env.BASEROW_URL+'669051/?user_field_names=true', {
     method: 'GET',
     headers: {
@@ -8,5 +9,5 @@ export default defineEventHandler(async (event) => {
     }
   })
 
-  return event
+  return query
 })
