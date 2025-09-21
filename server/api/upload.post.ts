d3b7a1f319c66dc135b640b0ba165ca8941d1b85
Nico-L <luchier@gmail.com>
2025-09-21 23:13:20 +0200

    working on upload

--- server/api/upload.post.ts
+++ server/api/upload.post.ts
@@ -2,14 +2,16 @@ import { Repo } from '~/types'
 
 export default defineEventHandler(async (event) => {
   const body = await readBody(event)
+  var dataFile = new FormData()
+    dataFile.append('file', body.files[0])
   const data = await $fetch<Repo[]>('https://api.baserow.io/api/user-files/upload-file/', {
     method: 'POST',
     headers: {
       'Authorization': 'Token ' + process.env.BASEROW_DEVOIRS,
       "Content-Type": "multipart/form-data"
     },
-    body: body
+    body: dataFile
   })
 
-  return body
+  return data
 })
