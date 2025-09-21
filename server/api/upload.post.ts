b9eccc9eb754d1de50a74935426044a0f6e6e10a
Nico-L <luchier@gmail.com>
2025-09-21 23:44:52 +0200

    no content type

--- server/api/upload.post.ts
+++ server/api/upload.post.ts
@@ -8,8 +8,7 @@ export default defineEventHandler(async (event) => {
   const data = await $fetch<Repo[]>('https://api.baserow.io/api/user-files/upload-file/', {
     method: 'POST',
     headers: {
-      'Authorization': 'Token ' + process.env.BASEROW_DEVOIRS,
-      'content-type': 'multipart/form-data'
+      'Authorization': 'Token ' + process.env.BASEROW_DEVOIRS
     },
     body: body
   })
