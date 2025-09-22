389d91d752a1f85f091819d314d236866fe276d5
Nico-L <nicolas@luchier.fr>
2025-09-22 09:20:19 +0200

    Update upload.post.ts : new formdata

--- server/api/upload.post.ts
+++ server/api/upload.post.ts
@@ -3,12 +3,14 @@ import { Repo } from '~/types'
 export default defineEventHandler(async (event) => {
   const form = await readMultipartFormData(event)
   console.log('form', form)
+  const formData = new FormData()
+  formData.append('file', form.data)
   const data = await $fetch<Repo[]>('https://api.baserow.io/api/user-files/upload-file/', {
     method: 'POST',
     headers: {
       'Authorization': 'Token ' + process.env.BASEROW_DEVOIRS
     },
-    body: form
+    body: formData
   })
 
   return data
