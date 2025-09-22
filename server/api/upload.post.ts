b2b5941ac425817cb9e5a126384a15ed0e9c6448
Nico-L <nicolas@luchier.fr>
2025-09-22 11:23:49 +0200

    Update upload.post.ts : log each data

--- server/api/upload.post.ts
+++ server/api/upload.post.ts
@@ -18,6 +18,7 @@ export default defineEventHandler(async (event) => {
         },
         body: formData
       })
+     console.log('data', data)
        retour.push(data) 
    })
   console.log('retour', retour)
