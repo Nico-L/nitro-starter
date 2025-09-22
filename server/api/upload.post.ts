2058c53d8c2a459130141d7b0eb98475615bb2a8
Nico-L <nicolas@luchier.fr>
2025-09-22 12:09:42 +0200

    Update upload.post.ts : loop forms

--- server/api/upload.post.ts
+++ server/api/upload.post.ts
@@ -4,24 +4,27 @@ export default defineEventHandler(async (event) => {
   const formDataBody = await readMultipartFormData(event)
   console.log('received data', formDataBody)
    // Append the data to a new FormData (need to convert Buffer into string / Blob)
+  var forms=[]
   var retour = []
-   formDataBody?.forEach(async (value) => {
-       console.log('bob ?')
+   formDataBody?.forEach((value) => {
        const formData = new FormData()
        if (value.name && value.data) {
          const blob = new Blob([value.data], { type: value.type });
          formData.append('file', blob)
        }
-       const data = await $fetch<Repo[]>('https://api.baserow.io/api/user-files/upload-file/', {
+     forms.push(formData)
+   })
+forms?.forEach(async (value) => {
+  const data = await $fetch<Repo[]>('https://api.baserow.io/api/user-files/upload-file/', {
         method: 'POST',
         headers: {
           'Authorization': 'Token ' + process.env.BASEROW_DEVOIRS
         },
-        body: formData
+        body: value
       })
      console.log('data', data)
-       retour.push(data) 
-   })
+     retour.push(data) 
+})
   console.log('retour', retour)
   return retour
 })
