e5e5ddcd8d5942bff84c9a988de24219b982ccae
Nico-L <nicolas@luchier.fr>
2025-09-22 11:14:51 +0200

    Update upload.post.ts log array received

--- server/api/upload.post.ts
+++ server/api/upload.post.ts
@@ -2,15 +2,15 @@ import { Repo } from '~/types'
 
 export default defineEventHandler(async (event) => {
   const formDataBody = await readMultipartFormData(event)
-  console.log('received data', formDataBody[0])
+  console.log('received data', formDataBody)
   const formData = new FormData()
    // Append the data to a new FormData (need to convert Buffer into string / Blob)
-         formDataBody?.forEach((value) => {
-             if (value.name && value.data) {
-               const blob = new Blob([value.data], { type: value.type });
-               formData.append('file', blob)
-             }
-         })
+   formDataBody?.forEach((value) => {
+       if (value.name && value.data) {
+         const blob = new Blob([value.data], { type: value.type });
+         formData.append('file', blob)
+       }
+   })
   const data = await $fetch<Repo[]>('https://api.baserow.io/api/user-files/upload-file/', {
     method: 'POST',
     headers: {
