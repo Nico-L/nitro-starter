dad345287b3a5c1ba47a6988e3c9fa78a068f689
Nico-L <nicolas@luchier.fr>
2025-09-22 12:44:21 +0200

    Update upload.post.ts redoing loop

--- server/api/upload.post.ts
+++ server/api/upload.post.ts
@@ -1,5 +1,15 @@
 import { Repo } from '~/types'
 
+async zeFetch (form) (
+  return $fetch<Repo[]>('https://api.baserow.io/api/user-files/upload-file/', {
+              method: 'POST',
+              headers: {
+                'Authorization': 'Token ' + process.env.BASEROW_DEVOIRS
+              },
+              body: form
+            })
+  )
+
 export default defineEventHandler(async (event) => {
   const formDataBody = await readMultipartFormData(event)
   console.log('received data', formDataBody)
@@ -8,29 +18,18 @@ export default defineEventHandler(async (event) => {
   var retour = []
    formDataBody?.forEach((value) => {
        const formData = new FormData()
-       if (value.name && value.data) {
+       if (value.data) {
          const blob = new Blob([value.data], { type: value.type });
          formData.append('file', blob)
-       }
-     forms.push(formData)
+       
+         try{
+            var data = await zeFetch(formData) 
+          } catch (e) {
+            console.log('error', e)
+          }
+         retour.push(data)
+     }
    })
-console.log('forms', forms)
-forms?.forEach( async (form) => {
-  console.log('bob ?')
-  try{
-    var data = await $fetch<Repo[]>('https://api.baserow.io/api/user-files/upload-file/', {
-          method: 'POST',
-          headers: {
-            'Authorization': 'Token ' + process.env.BASEROW_DEVOIRS
-          },
-          body: form
-        })
-  } catch (e) {
-    console.log('error', e)
-  }
- console.log('data', data)
- retour.push(data) 
-})
   console.log('retour', retour)
   return retour
 })
