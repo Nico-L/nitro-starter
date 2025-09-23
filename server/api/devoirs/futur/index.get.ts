24eee2d3e20e9e873a4af5c2bb4bb1e4a4565f4c
Nico-L <nicolas@luchier.fr>
2025-09-23 13:19:02 +0200

    Update index.get.ts : filter futur devoirs

--- server/api/devoirs/futur/index.get.ts
+++ server/api/devoirs/futur/index.get.ts
@@ -1,7 +1,7 @@
 import { Repo } from '~/types'
 
 export default defineEventHandler(async (event) => {
-  const data = await $fetch<Repo[]>(process.env.BASEROW_URL+'669051/?user_field_names=true&order_by=-date', {
+  const data = await $fetch<Repo[]>(process.env.BASEROW_URL+'669051/?user_field_names=true&order_by=-date&filter_date_date_is_after=UTC??today', {
     method: 'GET',
     headers: {
       'Authorization': 'Token ' + process.env.BASEROW_DEVOIRS
