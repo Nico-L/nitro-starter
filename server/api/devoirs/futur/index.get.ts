57716f16fd607d14ed52a7e2bd11071b3ba92df1
Nico-L <nicolas@luchier.fr>
2025-09-23 13:21:43 +0200

    Update index.get.ts set timezone

--- server/api/devoirs/futur/index.get.ts
+++ server/api/devoirs/futur/index.get.ts
@@ -1,7 +1,7 @@
 import { Repo } from '~/types'
 
 export default defineEventHandler(async (event) => {
-  const data = await $fetch<Repo[]>(process.env.BASEROW_URL+'669051/?user_field_names=true&order_by=-date&filter_date_date_is_after=UTC??today', {
+  const data = await $fetch<Repo[]>(process.env.BASEROW_URL+'669051/?user_field_names=true&order_by=-date&filter_date_date_is_after=Europe/Amsterdam??today', {
     method: 'GET',
     headers: {
       'Authorization': 'Token ' + process.env.BASEROW_DEVOIRS
