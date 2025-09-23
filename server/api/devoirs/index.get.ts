f2ee4ec223a16bbdd01ed0ad6dde2754a6825967
Nico-L <nicolas@luchier.fr>
2025-09-23 13:09:57 +0200

    Update index.get.ts

--- server/api/devoirs/index.get.ts
+++ server/api/devoirs/index.get.ts
@@ -1,1 +1,12 @@
+import { Repo } from '~/types'
 
+export default defineEventHandler(async (event) => {
+  const data = await $fetch<Repo[]>(process.env.BASEROW_URL+'669051/?user_field_names=true&order_by=-date', {
+    method: 'GET',
+    headers: {
+      'Authorization': 'Token ' + process.env.BASEROW_DEVOIRS
+    }
+  })
+
+  return data
+})
