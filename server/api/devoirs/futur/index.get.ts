bc02be71b13c527d741ba453cfafc4d385bd6e7c
Nico-L <nicolas@luchier.fr>
2025-09-23 13:28:52 +0200

    Update index.get.ts : fixed filter

--- server/api/devoirs/futur/index.get.ts
+++ server/api/devoirs/futur/index.get.ts
@@ -1,7 +1,12 @@
 import { Repo } from '~/types'
 
 export default defineEventHandler(async (event) => {
-  const data = await $fetch<Repo[]>(process.env.BASEROW_URL+'669051/?user_field_names=true&order_by=-date&filters={"filter_type":"AND","filters":[{"type":"date_is_after","field":"date","value":"Europe/Paris??today"}],"groups":[]}, {
+  var url = process.env.BASEROW_URL
+  url = url + "669051/?user_field_names=true"
+  const order = "order_by=-date"
+  const filtre = "filters=%7B%22filter_type%22%3A%22AND%22%2C%22filters%22%3A%5B%7B%22type%22%3A%22date_is_after%22%2C%22field%22%3A%22date%22%2C%22value%22%3A%22Europe%2FParis%3F%3Ftoday%22%7D%5D%2C%22groups%22%3A%5B%5D%7D"
+  const data = await $fetch<Repo[]>(url+"&"+order+"&"+filtre, 
+  {
     method: 'GET',
     headers: {
       'Authorization': 'Token ' + process.env.BASEROW_DEVOIRS
