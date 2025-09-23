3895553687f9b1fc669302e4dd904201c02df172
Nico-L <nicolas@luchier.fr>
2025-09-23 13:23:04 +0200

    Update index.get.ts

--- server/api/devoirs/futur/index.get.ts
+++ server/api/devoirs/futur/index.get.ts
@@ -1,7 +1,7 @@
 import { Repo } from '~/types'
 
 export default defineEventHandler(async (event) => {
-  const data = await $fetch<Repo[]>(process.env.BASEROW_URL+'669051/?user_field_names=true&order_by=-date&filter_date_date_is_after=Europe/Amsterdam??today', {
+  const data = await $fetch<Repo[]>(process.env.BASEROW_URL+'669051/?user_field_names=true&order_by=-date&filters={"filter_type":"AND","filters":[{"type":"date_is_after","field":"date","value":"Europe/Paris??today"}],"groups":[]}, {
     method: 'GET',
     headers: {
       'Authorization': 'Token ' + process.env.BASEROW_DEVOIRS
